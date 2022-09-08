d4cadf534a02f190fb017d7bae8b874c867698ec
Clement Nduonyi <cndigitech@gmail.com>
2022-09-08 01:16:31 +0100

    restructured workcard component

--- components/shared/WorkCard.js
+++ components/shared/WorkCard.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import {Row, Col, Button} from 'reactstrap';
+import { useRouter } from 'next/router';
 import {isAuthorized} from '@/utils/auth0';
 import ProjectCard from "@/components/ProjectCard";
 
 
 const WorkCard = () => {
     const [projects, setProjects] = useState([])
+    const router = useRouter();
 
     const getProjects = async () => {
         const response = await axios.get(process.env.project_API_URL + '/projects')
@@ -20,30 +22,31 @@ const WorkCard = () => {
 
     return(
         <div className="project-page-content">
+            <h3>projects I was working on recently</h3>
             <Row style={{width: '100%'}} className="mb-3">
-                <h3>projects I was working on recently</h3>
                 { projects.map(project =>
                     <Col 
                     onClick={() => router.push('/projects/[id]', `projects/${project._id}`)}
                     key={project._id}  md={{size: 4, offset: 0}}>
-                        <ProjectCard
-                        project = {project} 
-                        >
-                            {dataU && isAuthorized(dataU, 'admin') &&
-                                <>
-                                    <Button
-                                    onClick={(e) => {
-                                        e.stopPropagation()
-                                        router.push('/projects/[id]/edit', `projects/${project._id}/edit`)
-                                    }}
-                                    className="me-2" color="warning">Edit</Button>
-                                    <Button
-                                    onClick={(e) => _deleteproject(e, project._id)}
-                                    color="danger">Delete</Button>
-                                </>
-                            }
-                            
-                        </ProjectCard>
+                       <Card style={{width: '100%', height: '200px'}} className="project-card">
+                            <CardImg style={{width: '400px', height: '200px'}} className="" tag="h6">
+                                <img
+                                src={project.image.url} 
+                                alt= {`${project.title.substr(0, 20)}...`}
+                                style={{width: '100%', height: '100%'}} />
+                            </CardImg>
+                        
+                            <CardImgOverlay className="project-card-overlay">
+                                <CardTitle tag="h5" className="project-card-header" >
+                                    { project.title }
+                                </CardTitle>
+                                
+                                <CardText style={{color: '#000'}} className="project-card-description">
+                                    {`${ project.description.substr(0, 80) } Read more...`}
+                                </CardText>
+                                
+                            </CardImgOverlay>
+                        </Card>
                     </Col>
                     )
                 }
